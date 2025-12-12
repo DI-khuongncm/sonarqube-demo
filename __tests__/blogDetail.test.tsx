f398020b00fa272c7cb3b12329f70382caf5c51c
DI-khuongncm <nguyen.cong.manh.khuong@dataimpact.vn>
2025-12-12 10:13:13 +0700

    set up project

---
+++ __tests__/blogDetail.test.tsx
@@ -0,0 +1,15 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Page from "../app/blog/[slug]/page";
+import { ReactNode } from "react";
+
+it("App Router: Works with dynamic route segments", async () => {
+  const PageGenerated: ReactNode = await Page({
+    params: Promise.resolve({ slug: "Test" }),
+  });
+  render(PageGenerated);
+  expect(screen.getByRole("heading")).toHaveTextContent("Slug: Test");
+});
